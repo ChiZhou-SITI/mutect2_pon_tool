d2b87f30c8c71a9e49966bac15d00df7202a9f22
Shenglai Li <sli6@uchicago.edu>
2016-01-20 10:42:51 -0600

    update(tool.py): update mutect2_pon_tool.py

---
+++ mutect2-pon-tool/tools/mutect2_pon_tool.py
@@ -0,0 +1,34 @@
+
+import os
+import sys
+
+from cdis_pipe_utils import df_util
+from cdis_pipe_utils import pipe_util
+from cdis_pipe_utils import time_util
+
+def pon(uuid, bam_path, thread_count, reference_fasta_path, cosmic_path, target_interval, known_snp_vcf_path, engine, logger):
+  step_dir = os.getcwd()
+  bam_name = os.path.basename(bam_path)
+  bam_base, bam_ext = os.path.splitext(bam_name)
+  out_pon_vcf = os.path.join(step_dir, bam_base) + '_pon.vcf'
+  logger.info('out_pon_vcf=%s' % out_pon_vcf)
+  if pipe_util.already_step(step_dir, bam_name + '_MuTect2_PON', logger):
+    logger.info('already completed step `MuTect2 Panel Of Normal calling` of: %s' % bam_path)
+  else:
+    logger.info('running step `MuTect2 Panel Of Normal calling` of: %s' % bam_path)
+    home_dir = os.path.expanduser('~')
+    gatk_path = os.path.join(home_dir, 'tools', 'GenomeAnalysisTK.jar')
+    cmd = ['java', '-d64', '-jar', gatk_path, '-nct', str(thread_count), '-T', 'MuTect2', '-R', reference_fasta_path, '-I:tumor', bam_path, '--cosmic', cosmic_path, '--dbsnp', known_snp_vcf_path, '--artifact_detection_mode', '-L', target_interval, '-o', out_pon_vcf]
+    output = pipe_util.do_command(cmd, logger)
+
+    #store time/mem
+    df = time_util.store_time(uuid, cmd, output, logger)
+    df['out_pon_vcf'] = out_pon_vcf
+    df['bam_path'] = bam_path
+    df['thread_count'] = thread_count
+    table_name = 'time_mem_gatk_mutect2_pon'
+    unique_key_dict = {'uuid': uuid, 'bam_path': bam_path, 'thread_count': thread_count, 'out_pon_vcf': out_pon_vcf}
+    df_util.save_df_to_sqlalchemy(df, unique_key_dict, table_name, engine, logger)
+    pipe_util.create_already_step(step_dir, bam_name + '_MuTect2_PON', logger)
+    logger.info('completed running step `MuTect2 Panel Of Normal calling` of: %s' % bam_path)
+  return
