e665a2c28e2a6f0cd96eae134db712be5962bf01
Shenglai Li <sli6@uchicago.edu>
2016-01-21 11:02:06 -0600

    fix(RTC.py): fix undefined variables

--- mutect2-pon-tool/tools/RealignerTargetCreator.py
+++ mutect2-pon-tool/tools/RealignerTargetCreator.py
@@ -13,9 +13,9 @@ def rtc(uuid, bam_path, thread_count, reference_fasta_path, known_indel_vcf_path
   out_intervals_path = os.path.join(step_dir, bam_base) + '.intervals'
   logger.info('out_intervals_path=%s' % out_intervals_path)
   if pipe_util.already_step(step_dir, uuid + '_RealignerTargetCreator', logger):
-    logger.info('already completed step `RealignerTargetCreator` of: %s' % bam_path_list)
+    logger.info('already completed step `RealignerTargetCreator` of: %s' % bam_path)
   else:
-    logger.info('running step `RealignerTargetCreator` of: %s' % bam_path_list)
+    logger.info('running step `RealignerTargetCreator` of: %s' % bam_path)
     home_dir = os.path.expanduser('~')
     gatk_path = os.path.join(home_dir, 'tools', 'GenomeAnalysisTK.jar')
     cmd = ['java', '-Djava.io.tmpdir=/tmp/job_tmp', '-d64', '-jar', gatk_path, '-nt', str(thread_count), '-T', 'RealignerTargetCreator', '-R', reference_fasta_path, '-known', known_indel_vcf_path, '-I', bam_path, '-o', out_intervals_path]
@@ -31,5 +31,5 @@ def rtc(uuid, bam_path, thread_count, reference_fasta_path, known_indel_vcf_path
     df_util.save_df_to_sqlalchemy(df, unique_key_dict, table_name, engine, logger)
 
     pipe_util.create_already_step(step_dir, uuid + '_RealignerTargetCreator', logger)
-    logger.info('completed running step `RealignerTargetCreator` of: %s' % bam_path_list)
+    logger.info('completed running step `RealignerTargetCreator` of: %s' % bam_path)
   return
